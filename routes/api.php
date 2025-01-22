49c54189fa17fd8e41d684bebb9371774418c779
Fernando Aguilar <feraguilar6985@gmail.com>
2025-01-21 18:18:06 -0600

    Add authentication middleware to user routes for JWT validation

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\userController;
+use App\Http\Middleware\AuthMiddleware;
 
-Route::apiResource('users', userController::class);
+Route::apiResource('users', userController::class)->middleware([AuthMiddleware::class]);
 Route::post('login', [userController::class, 'login']);
