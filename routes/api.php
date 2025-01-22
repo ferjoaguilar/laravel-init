cb52aac0f57502725ccd73cafca6a4602ea0a84a
Fernando Aguilar <feraguilar6985@gmail.com>
2025-01-21 22:24:56 -0600

    Refactor user routes to replace apiResource with explicit route definitions and apply authentication middleware selectively

--- routes/api.php
+++ routes/api.php
@@ -5,5 +5,12 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\userController;
 use App\Http\Middleware\AuthMiddleware;
 
-Route::apiResource('users', userController::class)->middleware([AuthMiddleware::class]);
+/* Route::apiResource('users', userController::class)->middleware(AuthMiddleware::class); */
+
+Route::get('users', [userController::class, 'index']);
+Route::post('users', [userController::class, 'store']);
+Route::get('users/{id}', [userController::class, 'show']);
+Route::put('users/{id}', [userController::class, 'update'])->middleware(AuthMiddleware::class);
+Route::delete('users/{id}', [userController::class, 'destroy'])->middleware(AuthMiddleware::class);
+
 Route::post('login', [userController::class, 'login']);
